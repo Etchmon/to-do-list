3285c9d0735dc59293aedb9407261219c0f9f269
James Etchells <etch.rush@gmail.com>
2022-03-02 14:11:02 -0800

    Add started the displayController Add createDom function Edit index.js removed component function, replaced with displayController createDom call

---
+++ src/displayController.js
@@ -0,0 +1,20 @@
+import nav from './nav';
+
+// Export a auto running displayer controlle module
+// ----Functions to add-------
+// createDOM to create the html elements, set their id's and classes || fill with dummy data if no data available
+
+export const displayController = (() => {
+
+    const createDom = () => {
+
+        const element = document.createElement('section');
+
+        element.setAttribute('id', 'content');
+        element.appendChild(nav());
+
+        document.body.appendChild(element);
+    }
+
+    return { createDom }
+})();
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import { displayController } from './displayController';
 
 
-function component() {
-    const element = document.createElement('div');
-
-    // Lodash, currently included via a script, is required for this line to work
-    // Lodash, now imported by this script
-    element.innerHTML = 'watch this';
-
-    return element;
-}
-
-document.body.appendChild(component());
+displayController.createDom();
